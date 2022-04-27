ea71fe3b951692c1cdb255a8c68ffdb728aa4b3d
seunghyun <shawnyoo.des@gmail.com>
2022-04-27 16:50:41 +0900

    refactoring

--- js/main.js
+++ js/main.js
@@ -60,16 +60,3 @@ function includeHTML(callback) {
         //callback();
     }, 0);
 }
-
-/* date calculator */
-function counter() {
-    var dday = new Date("May 28, 2018, 00:00:00").getTime();
-
-    setInterval(function () {
-        var now = new Date();
-        var distance = now - dday;
-        var d = Math.floor(distance / (1000 * 60 * 60 * 24));
-        $('#dday').html(d + " days")
-    }, 1000);
-}
-counter();
